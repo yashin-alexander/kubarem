833d95a75dd8936c18fdc391aa38f3c7f42eae49
Alexander Yashin <alexandr.yashin@emlid.com>
2020-04-12 21:39:33 +0300

    src: input: add good keys\text callbacks
    All this stuff here is a hook around a glfwSetKeyCallback callback
    function. A pressed keys are should to be stored somewhere, but
    callback signature does not provide any `class instance` or `user data`
    argument. So, _BaseInput class ontains glfw callbacks which are
    redirects all calls to Input class.

--- src/input.cpp
+++ src/input.cpp
@@ -2,29 +2,90 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+extern "C" {
+    #include "log.h"
+}
 
-class Input{
-private:
-    GLFWwindow *_window;
+/*
+ * All this stuff here is a hook around a glfwSetKeyCallback callback function.
+ * We need to store ppressed keys somewhere, but callback signature does not
+ * provide any `class instance` or `user data` argument. So, _BaseInput class
+ * contains glfw callbacks which are redirects all calls to Input class.
+*/
 
-    static void _TextInputCb(GLFWwindow* window, unsigned int codepoint)
+#ifndef INPUT_H
+#define INPUT_H
+
+class _BaseInput
+{
+    virtual void _KeysSetupCb(GLFWwindow *window, int key, int scancode, int action, int mods) = 0;
+    virtual void _TextInputCb(GLFWwindow *window, unsigned int codepoint) = 0;
+
+public:
+    static _BaseInput *event_handling_instance;
+
+//    virtual ~_BaseInput();
+
+    virtual void setEventHandling()
     {
-        char key_name;
-        key_name = std::cin.narrow(codepoint, '0');
-        std::cout << "Key press detected: " << key_name << std::endl;
+        event_handling_instance = this;
     }
 
-    static void _KeysSetupCb(GLFWwindow* window, int key, int scancode, int action, int mode)
+    static void KeysSetupCb(GLFWwindow *window, int key, int scancode, int action, int mods)
+    {
+        if(event_handling_instance)
+            event_handling_instance->_KeysSetupCb(window, key, scancode, action, mods);
+    }
+
+    static void TextInputCb(GLFWwindow *window, unsigned int codepoint)
+    {
+        if(event_handling_instance)
+            event_handling_instance->_TextInputCb(window, codepoint);
+    }
+};
+
+
+_BaseInput * _BaseInput::event_handling_instance;
+
+
+class Input : _BaseInput
+{
+private:
+    GLFWwindow *_window;
+    virtual void _KeysSetupCb(GLFWwindow* window, int key, int scancode, int action, int mode)
     {
         if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
             glfwSetWindowShouldClose(window, GL_TRUE);
+        if (key >= 0 && key < 1024)
+        {
+            if (action == GLFW_PRESS)
+                this->Keys[key] = GL_TRUE;
+            else if (action == GLFW_RELEASE)
+            {
+                this->Keys[key] = GL_FALSE;
+            }
+        }
+    }
+
+    virtual void _TextInputCb(GLFWwindow* window, unsigned int codepoint)
+    {
+        char key_name;
+        key_name = std::cin.narrow(codepoint, '0');
+        log_dbg("Key press detected: %c", key_name);
     }
 
 public:
+    GLboolean Keys[1024] = {GL_FALSE};
+
     Input(GLFWwindow *window):
     _window(window)
     {
-        glfwSetKeyCallback(this->_window, this->_KeysSetupCb);
-        glfwSetCharCallback(this->_window, this->_TextInputCb);
+        this->setEventHandling();
+        glfwSetKeyCallback(window, KeysSetupCb);
+        glfwSetCharCallback(window, TextInputCb);
     }
+
+//    virtual ~Input();
 };
+
+#endif
