e4a61d16607dd3db6f3aff664623c5b5af75b9c6
Alexander Yashin <alexandr.yashin@emlid.com>
2020-04-21 00:26:23 +0300

    src: shaders: update shaders

--- src/shaders/default_vs.glsl
+++ src/shaders/default_vs.glsl
@@ -2,12 +2,13 @@
 
 layout (location = 0) in vec4 vertex; // <vec2 position, vec2 texCoords>
 
+uniform mat4 view;
 uniform mat4 model;
 uniform mat4 projection;
 
 void main()
 {
-//    gl_Position = projection * model * vec4(vertex.xy, 0.0, 1.0);
-    gl_Position = model * vec4(vertex.xy, 0.0, 1.0);
+    gl_Position = projection * view * model * vec4(vertex.xy, 0.0, 1.0);
+//    gl_Position = model * vec4(vertex.xy, 0.0, 1.0);
 //    gl_Position = vec4(vertex.xy, 0.0, 1.0);
 }
