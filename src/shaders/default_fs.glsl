cf71336e7e4e5755910347537e26da0c2ffa2693
Alexander Yashin <alexandr.yashin@emlid.com>
2020-05-02 22:18:04 +0300

    src: update shaders for 3d models display

--- src/shaders/default_fs.glsl
+++ src/shaders/default_fs.glsl
@@ -1,12 +1,11 @@
 #version 330 core
-in vec2 TexCoord;
+out vec4 FragColor;
 
-out vec4 color;
+in vec2 TexCoords;
 
-uniform sampler2D ourTexture1;
-uniform sampler2D ourTexture2;
+uniform sampler2D texture_diffuse1;
 
 void main()
 {
-    color = mix(texture(ourTexture1, TexCoord), texture(ourTexture2, TexCoord), 0.2);
+    FragColor = texture(texture_diffuse1, TexCoords);
 }
