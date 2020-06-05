f10c56d29470d587ae97695a410f8f78f48742ed
Alexander Yashin <alexandr.yashin@emlid.com>
2020-06-05 04:27:02 +0300

    src: camera: third_person: initial rotation support

--- src/camera.cpp
+++ src/camera.cpp
@@ -7,8 +7,8 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 #include <vector>
+#include <cmath>
 #include "log.h"
-
 
 enum Camera_Movement {
     FORWARD,
@@ -48,15 +48,6 @@ public:
         updateCameraVectors();
     }
 
-    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
-    {
-        Position = glm::vec3(posX, posY, posZ);
-        WorldUp = glm::vec3(upX, upY, upZ);
-        Yaw = yaw;
-        Pitch = pitch;
-        updateCameraVectors();
-    }
-
     glm::mat4 GetViewMatrix()
     {
         return glm::lookAt(Position, Position + Front, Up);
@@ -75,7 +66,7 @@ public:
             Position += Right * velocity;
     }
 
-    void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
+    virtual void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
     {
         xoffset *= MouseSensitivity;
         yoffset *= MouseSensitivity;
@@ -104,7 +95,7 @@ public:
             Zoom = 45.0f;
     }
 
-private:
+protected:
     void updateCameraVectors()
     {
         // Calculate the new Front vector
@@ -118,4 +109,58 @@ private:
         Up    = glm::normalize(glm::cross(Right, Front));
     }
 };
+
+
+class ThirdPersonCamera: public Camera
+{
+
+public:
+    GLfloat springArmLength = 40.f;
+
+    ThirdPersonCamera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f)) : Camera(position)
+    {
+        setPosition(position);
+        log_dbg("Camera position = %f %f %f", position[0], position[1], position[2]);
+        updateCameraVectors();
+    }
+
+
+    virtual void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
+    {
+        xoffset *= MouseSensitivity;
+        yoffset *= MouseSensitivity;
+
+        Yaw   -= xoffset;
+        Pitch += yoffset;
+
+        if (abs(xoffset) > 0.000001){
+            GLfloat hipo = sqrt(2 * pow(springArmLength, 2) - 2 * pow(springArmLength, 2) * glm::cos(glm::radians(xoffset)));
+            // cosine theorem
+            GLfloat x_off = glm::sin(glm::radians((180 - xoffset) / 2)) / hipo;
+            GLfloat y_off = glm::cos(glm::radians((180 - xoffset) / 2)) / hipo;
+            log_dbg("Yaw %f Offset %f cos %f Hipo %f", Yaw, xoffset, glm::cos(glm::radians(xoffset)), hipo);
+            log_dbg("X %f Y %f", x_off, y_off);
+        }
+
+//        GLfloat pos_offset = (this->springArmLength / tan(Yaw));
+//        log_dbg("Yaw % f ARM %f", tan(Yaw), pos_offset);
+//        Position += (0.0f, -pos_offset, 0.0f);
+
+        if (constrainPitch)
+        {
+            if (Pitch > 89.0f)
+                Pitch = 89.0f;
+            if (Pitch < -89.0f)
+                Pitch = -89.0f;
+        }
+
+        updateCameraVectors();
+    }
+
+
+    void setPosition(glm::vec3 position){
+        this->Position = position + glm::vec3(0.0f, 0.0f, springArmLength);
+    }
+};
+
 #endif
