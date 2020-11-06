6ee9a0cb81357df47a7cf3d57225b4959739d1c8
Alexander Yashin <alexandr.yashin@emlid.com>
2020-11-06 23:21:30 +0300

    src: introduce particle class

---
+++ src/particles/particle.cpp
@@ -0,0 +1,43 @@
+#include "particles/particle.h"
+#include "log.h"
+
+
+Particle::Particle(ParticleParameters parameters)
+{
+    setParameters(parameters);
+}
+
+
+void Particle::setParameters(ParticleParameters new_parameters) {
+    this->parameters = new_parameters;
+    elapsed_time = 0;
+}
+
+const glm::vec3& Particle::getPosition() const {
+    return parameters.position;
+}
+
+GLfloat Particle::getRotation() const {
+    return parameters.rotation;
+}
+
+GLfloat Particle::getScale() const {
+    return parameters.scale;
+}
+
+void Particle::update(GLfloat delta_time) {
+    if (elapsed_time < parameters.life_length) {
+        // update rotation
+        parameters.rotation += parameters.rotation * delta_time;
+
+        // update velocity u = u1 + at;
+        parameters.velocity.y = parameters.velocity.y - delta_time * parameters.gravity_effect;
+        parameters.position += delta_time * parameters.velocity;
+        elapsed_time += delta_time;
+    }
+}
+
+
+GLboolean Particle::isActive() const {
+    return elapsed_time < parameters.life_length;
+}
---
+++ src/particles/particle.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "glm/glm.hpp"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/gtc/matrix_transform.hpp>
+
+struct ParticleParameters {
+    glm::vec3 position;
+    glm::vec3 velocity;
+    GLfloat gravity_effect;
+    GLfloat life_length;
+    GLfloat rotation;
+    GLfloat scale;
+};
+
+class Particle
+{
+public:
+    Particle(ParticleParameters parameters);
+    ~Particle() = default;
+
+    void setParameters(ParticleParameters parameters);
+
+    const glm::vec3 &getPosition() const;
+
+    GLfloat getRotation() const;
+
+    GLfloat getScale() const;
+
+    void update(GLfloat delta_time);
+
+    GLboolean isActive() const;
+
+protected:
+    ParticleParameters parameters;
+
+    GLfloat elapsed_time = 0;
+};
+
