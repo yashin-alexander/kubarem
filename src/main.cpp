46e76f1e02f3a4e8405f956d5b55b636ebc16d5c
Desman735 <desman735.dev@gmail.com>
2020-11-24 15:49:38 +0300

    Update main OpenGL version to 4.3

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main()
     auto init_result = glfwInit();
     assert(init_result == GLFW_TRUE);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
