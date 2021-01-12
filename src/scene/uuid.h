01f9e9cafec942c5e157a07e8ee90dd2ae5cf217
Alexander Yashin <alexandr.yashin@emlid.com>
2021-01-12 08:15:10 +0300

    src: ecs: introduce objects UUIDs

---
+++ src/scene/uuid.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <random>
+#include <sstream>
+
+
+namespace UUID {
+    static std::random_device              rd;
+    static std::mt19937                    gen(rd());
+    static std::uniform_int_distribution<> dis(0, 15);
+    static std::uniform_int_distribution<> dis2(8, 11);
+
+    std::string generate_uuid_v4();
+}
