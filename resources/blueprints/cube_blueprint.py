266837427b0c13312fcdca229cb71e2f6a14f3fa
Alexander Yashin <alexandr.yashin@emlid.com>
2021-01-13 10:23:59 +0300

    resources: introduce scripts python module

---
+++ resources/blueprints/cube_blueprint.py
@@ -0,0 +1,15 @@
+import math
+import time
+from kubarem import PyTransformComponent, Vec3
+
+
+class DerivedPyTransformComponent(PyTransformComponent):
+    def __init__(self, pos, siz):
+        super(DerivedPyTransformComponent, self).__init__(pos, siz)
+        # print("Position x:{} y:{} z:{}".format(self.position.x, self.position.y, self.position.z))
+        # print("Size x:{} y:{} z:{}".format(self.size.x, self.size.y, self.size.z))
+
+    def on_update(self, delta_time):
+        self.position.x += math.sin(time.time())
+        self.position.z += math.cos(time.time())
+
