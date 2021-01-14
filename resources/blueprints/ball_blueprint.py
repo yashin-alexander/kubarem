7c923567b61277c77c3fda35ae0fc2facd9d06cd
Alexander Yashin <alexandr.yashin@emlid.com>
2021-01-14 04:56:31 +0300

    resources: add scene bp

--- resources/blueprints/ball_blueprint.py
+++ resources/blueprints/ball_blueprint.py
@@ -1,19 +1,21 @@
 import math
 import time
-from kubarem import PyTransformComponent, Vec3
+from kubarem import Vec3, PyEntity
 
 
-class DerivedPyTransformComponent(PyTransformComponent):
-    def __init__(self, pos, siz):
-        super(DerivedPyTransformComponent, self).__init__(pos, siz)
-
-    def on_create(self):
-        pass
+class DerivedPyEntity(PyEntity):
+    def __init__(self, uuid, tag, transform):
+        super(DerivedPyEntity, self).__init__(uuid, tag, transform)
 
     def on_update(self, delta_time):
-        self.position.y = 10 - 5 * math.sin(self.position.x * time.time() / 100)
-        # self.position.z = -160 - 5 * math.cos(self.position.x * time.time() / 30)
+        self.transform.position.y = 10 - 5 * math.sin(self.transform.position.x * time.time() / 100)
+        self.transform.position.z = -160 - 5 * math.cos(self.transform.position.x * time.time() / 30)
+
+    def on_create(self):
+        super(DerivedPyEntity, self).on_create()
+        print("Entity {}:{} on create".format(self.uuid.uuid, self.tag.tag))
 
     def on_destroy(self):
-        pass
+        super(DerivedPyEntity, self).on_destroy()
+        print("Entity {} on destroy".format(self.tag.tag))
 
