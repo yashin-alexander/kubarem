caad11a166cdaf447fe588424907d968fb0ee637
Alexander Yashin <alexandr.yashin@emlid.com>
2021-02-22 03:08:00 +0300

    app: add GOAP usage

--- app/app.cpp
+++ app/app.cpp
@@ -1,7 +1,3 @@
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
@@ -17,13 +13,19 @@
 #include "renderer/framebuffer.h"
 #include "particles/particle_controller.h"
 #include "scene/scene_serializer.h"
-
+#include "scene/uuid.h"
+#include "ai/world_state.h"
+#include "ai/actions/ai_action_follow.h"
 
 const unsigned int SCR_WIDTH = 1280;
 const unsigned int SCR_HEIGHT = 700;
 
 
 namespace kubarem {
+    int GOAPHeuristic(const goap::WorldState &now, const goap::WorldState &goal) {
+        return now.distanceTo(goal) * 2;
+    }
+
     class App {
     private:
         GLFWwindow *window;
@@ -34,7 +36,6 @@ namespace kubarem {
         App() {
             this->OpenglSetup();
             this->CreateSceneLayout();
-            this->ImGuiSetup();
         }
 
         void OpenglSetup() {
@@ -102,9 +103,9 @@ namespace kubarem {
             auto *triangleSphereModel = new Model("resources/objects/sphere/triangle/sphere.obj", false);
             auto *thirdPersonCharacterModel = new Model("resources/objects/sphere/disco/sphere.obj", false);
 
-            auto * audioPositioned = new AudioPositioned("s.mp3");
-            auto * audioBackground = new AudioBackground("s.mp3");
-            auto * audioSpeech = new AudioSpeech("", (unsigned int) 530, (float) 10, (float) 0.5, (int) KW_NOISE);
+            auto *audioPositioned = new AudioPositioned("s.mp3");
+            auto *audioBackground = new AudioBackground("s.mp3");
+            auto *audioSpeech = new AudioSpeech("", (unsigned int) 530, (float) 10, (float) 0.5, (int) KW_NOISE);
 
             std::map<std::string, Model> models_map = {
                     {std::string("resources/objects/cyborg/cyborg.obj"),          *cyborgModel},
@@ -134,12 +135,14 @@ namespace kubarem {
             tpc.addComponent<kubarem::ModelComponent>("resources/objects/sphere/sphere.obj");
             tpc.addComponent<kubarem::TransformComponent>(glm::vec3(1, 2, 3), glm::vec3(4));
             tpc.addComponent<kubarem::ShaderProgramComponent>("src/shaders/main_vs.glsl");
+            auto tpc_uuid = tpc.getComponent<kubarem::UuidComponent>().uuid;
 
-            /*
+//            /*
             kubarem::Entity cyborgEntity = scene_->CreateEntity("Cyborg");
             cyborgEntity.addComponent<kubarem::ModelComponent>("resources/objects/cyborg/cyborg.obj");
             cyborgEntity.addComponent<kubarem::TransformComponent>(glm::vec3(0, 0, -100), glm::vec3(6, 6, 6));
             cyborgEntity.addComponent<kubarem::ShaderProgramComponent>("src/shaders/object_vs.glsl");
+            cyborgEntity.addComponent<kubarem::PyScriptComponent>("resources.blueprints.cube_blueprint");
 
             for (int i = 0; i < 525; i++) {
                 std::string name = std::string("Ball") + std::to_string(i);
@@ -156,7 +159,16 @@ namespace kubarem {
             cubeEntity.addComponent<kubarem::TransformComponent>(glm::vec3(0, 0, -40), glm::vec3(10));
             cubeEntity.addComponent<kubarem::ShaderProgramComponent>("src/shaders/object_vs.glsl");
             cubeEntity.addComponent<kubarem::AudioPositionedComponent>(audioPositioned);
-            cubeEntity.addComponent<kubarem::PyScriptComponent>("resources.blueprints.cube_blueprint");
+            {
+                goap::WorldState goal("Goal state");
+                goal.setFact(tpc_uuid.c_str(), "Dead", true);
+                goap::WorldState initial_state("Initial state");
+                initial_state.setFact(tpc_uuid.c_str(), "Dead", false);
+
+                std::vector<const goap::Action *> actions;
+                actions.push_back(new AIActionFollow("Follow tpc", 1, tpc));
+                cubeEntity.addComponent<kubarem::AIComponent>(actions, initial_state, goal, GOAPHeuristic);
+            }
 
             kubarem::Entity floorEntity = scene_->CreateEntity("Floor");
             floorEntity.addComponent<kubarem::CubeObjectComponent>("resources/textures/background.png");
@@ -167,44 +179,19 @@ namespace kubarem {
             particlesEmitterEntity.addComponent<kubarem::ParticlesComponent>(particles_parameters, (uint32_t) 1000);
             particlesEmitterEntity.addComponent<kubarem::ShaderProgramComponent>("src/shaders/particle_vs.glsl");
 
-
-        kubarem::Entity audio_background = scene_->CreateEntity("AudioBackground");
-        audio_background.addComponent<kubarem::AudioBackgroundComponent>(audioBackground);
-             */
+            kubarem::Entity audio_background = scene_->CreateEntity("AudioBackground");
+            audio_background.addComponent<kubarem::AudioBackgroundComponent>(audioBackground);
+//             */
 
 //        kubarem::Entity audioSpeech = scene_->CreateEntity("AudioSpeech");
 //        audioSpeech.addComponent<kubarem::AudioSpeechComponent>(&soloud_, "You will die! I kill you",
 //                                                                (unsigned int) 530, (float) 10, (float) 0.5,
 //                                                                (int) KW_NOISE);
 
-            SceneSerializer serializer(scene_);
-            serializer.Deserialize("scene.yaml");
+//            SceneSerializer serializer(scene_);
+//            serializer.Deserialize("scene.yaml");
         }
 
-
-        void ImGuiSetup() {
-            IMGUI_CHECKVERSION();
-            ImGui::CreateContext();
-            ImGuiIO &io = ImGui::GetIO();
-            (void) io;
-            io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
-            io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
-            io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
-
-            // Setup Dear ImGui style
-            ImGui::StyleColorsDark();
-
-            // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
-            ImGuiStyle &style = ImGui::GetStyle();
-            if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
-                style.WindowRounding = 0.0f;
-                style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-            }
-            // Setup Platform/Renderer embeddings
-            ImGui_ImplGlfw_InitForOpenGL(window, true);
-            const char *glsl_version = "#version 130";
-            ImGui_ImplOpenGL3_Init(glsl_version);
-        }
 
         void Run() {
             static GLfloat deltaTime = 0.0f;
@@ -217,7 +204,7 @@ namespace kubarem {
 
                 this->OnUpdate(deltaTime);
                 this->OnOpenglRender(deltaTime);
-                this->OnImGuiRender(deltaTime);
+//                this->OnImGuiRender(deltaTime);
 
                 _flush_log();
             }
@@ -226,6 +213,7 @@ namespace kubarem {
 
         void OnUpdate(float ts) {
             AudioCore::update_3d_audio();
+            scene_->OnAIUpdateRuntime(ts);
             scene_->OnUpdateRuntime(ts);
         }
 
@@ -237,100 +225,7 @@ namespace kubarem {
             glfwPollEvents();
         }
 
-        void OnImGuiRender(float ts) {
-            ImGui_ImplOpenGL3_NewFrame();
-            ImGui_ImplGlfw_NewFrame();
-            ImGui::NewFrame();
-            // -------
-
-            static bool dockingEnabled = true;
-            static bool dockspaceOpen = true;
-            static bool opt_fullscreen_persistant = true;
-            bool opt_fullscreen = opt_fullscreen_persistant;
-            static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
-
-            // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
-            // because it would be confusing to have two docking targets within each others.
-            ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-            if (opt_fullscreen) {
-                ImGuiViewport *viewport = ImGui::GetMainViewport();
-                ImGui::SetNextWindowPos(viewport->Pos);
-                ImGui::SetNextWindowSize(viewport->Size);
-                ImGui::SetNextWindowViewport(viewport->ID);
-                ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-                ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-                window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize |
-                                ImGuiWindowFlags_NoMove;
-                window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-            }
-
-            // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background and handle the pass-thru hole, so we ask Begin() to not render a background.
-            if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
-                window_flags |= ImGuiWindowFlags_NoBackground;
-
-            // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
-            // This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
-            // all active windows docked into it will lose their parent and become undocked.
-            // We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
-            // any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
-            ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-            ImGui::Begin("DockSpace Demo", &dockspaceOpen, window_flags);
-            ImGui::PopStyleVar();
-
-            if (opt_fullscreen)
-                ImGui::PopStyleVar(2);
-
-            // DockSpace
-            ImGuiIO &io = ImGui::GetIO();
-            if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
-                ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-                ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
-            }
-
-            if (ImGui::BeginMenuBar()) {
-                if (ImGui::BeginMenu("File")) {
-                    // Disabling fullscreen would allow the window to be moved to the front of other windows,
-                    // which we can't undo at the moment without finer window depth/z control.
-                    //ImGui::MenuItem("Fullscreen", NULL, &opt_fullscreen_persistant);
-
-                    if (ImGui::MenuItem("Exit")) this->Close();
-                    ImGui::EndMenu();
-                }
-
-                ImGui::EndMenuBar();
-            }
-
-            ImGui::Begin("Settings");
-
-            ImGui::Text("Renderer2D Stats:");
-            ImGui::Text("Draw Calls: %d", 5);
-            ImGui::Text("Quads: %d", 1);
-            ImGui::Text("Vertices: %d", 1);
-            ImGui::Text("Indices: %d", 1);
-
-            ImGui::End();
-
-            ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{0, 0});
-            ImGui::PopStyleVar();
-            ImGui::End();
-
-            // Rendering
-            ImGui::Render();
-            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-            io = ImGui::GetIO();
-            if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
-                GLFWwindow *backup_current_context = glfwGetCurrentContext();
-                ImGui::UpdatePlatformWindows();
-                ImGui::RenderPlatformWindowsDefault();
-                glfwMakeContextCurrent(backup_current_context);
-            }
-        }
-
         void Close() {
-            ImGui_ImplOpenGL3_Shutdown();
-            ImGui_ImplGlfw_Shutdown();
-            ImGui::DestroyContext();
             delete scene_;
             glfwDestroyWindow(window);
             glfwTerminate();
